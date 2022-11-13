10640b1fbde4b49d55bc8e3b9bad40938e631c7c
MaksPunch <maks.punch@yandex.ru>
2022-11-13 20:15:47 +0300

    '.'

--- app.js
+++ app.js
@@ -2,28 +2,26 @@ const express = require('express')
 
 const app = express();
 const { faker } = require('@faker-js/faker');
-faker.locale = 'ru';
-let User = [];
 
 function createRandomUser() {
-  return {
-    fullName: faker.name.fullName(),
-    adress: faker.address.streetAddress(true),
-    birthdate: faker.date.birthdate(),
-    vehicle: faker.vehicle.vehicle(),
-    phone: faker.phone.number('+7 ### ### ## ##')
-  }
+  let User = [];
+  Array.from({ length: 100 }).forEach( () => {
+    User.push({
+      fullName: faker.name.fullName(),
+      adress: `г. ${faker.address.city()}, ${faker.address.streetAddress(true)}`,
+      birthdate: faker.date.birthdate(),
+      vehicle: faker.vehicle.vehicle(),
+      phone: faker.phone.number('+7 ### ### ## ##')
+    }); 
+});
+  return { "users": User };
 }
 
-Array.from({ length: 100 }).forEach(() => {
-  User.push(createRandomUser());
-});
-
 app.use('/user', function (request, response) {
-  response.json(User);
+  response.status(200).type('text/plain')
+  response.send(JSON.stringify(createRandomUser(), null, '\t'));
 })
 
 app.listen(3000, () => {
   console.log('server started')
 })
-
